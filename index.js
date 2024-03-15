4d87bd4ec3de67e9a285b8711560324577749add
Ally <allyy.noel@gmail.com>
2024-03-15 18:52:50 -0400

    trial1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs'
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -9,15 +8,10 @@ async function getBooks() {
   .from('books')
   .select('*')
 
-  if (error) {
-    console.error('Error fetching books:', error.message);
-    return;
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<li>${book.title}</li>`;
   }
-
-  let bookList = document.getElementById('books');
-  books.forEach(book => {
-    bookList.innerHTML += `<li>${book.title} - ${book.author} - ${book.isbn}</li>`;
-  });
 }
 
 getBooks();
