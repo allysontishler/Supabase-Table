b19c17e7382af2d4ebadb040624973b00791652c
Ally <allyy.noel@gmail.com>
2024-03-15 19:04:59 -0400

    js-3columns

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 async function getBooks() {
   let { data: books, error } = await supabase
   .from('books')
-  .select('*')
+  .select('title, author, isbn')
 
   for (let book of books) {
     let bookList = document.getElementById('books');
-    bookList.innerHTML += `<li>${book.title}</li>`;
+    bookList.innerHTML += `<li>${book.title} - ${book.author} - ${book.isbn}</li>`;
   }
 }
 
