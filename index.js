8efe002ecc2554aa0b71f36fe5346eef29cc3aba
Ally <allyy.noel@gmail.com>
2024-03-15 18:23:29 -0400

    changes-js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getBooks() {
+async function populateTable() {
   let { data: books, error } = await supabase
     .from('books')
     .select('*');
@@ -27,4 +27,4 @@ async function getBooks() {
   });
 }
 
-getBooks();
+populateTable();
