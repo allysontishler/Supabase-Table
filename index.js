8a1041161ee8dab64e9f299f99bf2cb11e10476d
Ally <allyy.noel@gmail.com>
2024-03-15 19:47:11 -0400

    const

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs'
-const supabase = createClient(supabaseUrl, supabaseKey)
-
 const books = [
   { title: "The Courage To Be Disliked", author: "Ichiro Kishimi and Fumitake Koga", isbn: "978-1501197277" },
   { title: "Crying in H Mart", author: "Michelle Zauner", isbn: "978-0525657743" },
