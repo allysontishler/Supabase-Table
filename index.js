5daece58ff7000bfefe6133d63460d520f8a8294
Ally <allyy.noel@gmail.com>
2024-03-15 18:00:55 -0400

    initial-edits

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add JavaScript below
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function populateTable() {
+    const { data, error } = await supabase.from('books').select('*');
+
+    if (error) {
+      console.error('Error fetching books:', error.message);
+      return;
+    }
+
+    const tbody = document.querySelector('#booksTable tbody');
+    data.forEach(book => {
+      tbody.innerHTML += `
+        <tr>
+          <td>${book.Title}</td>
+          <td>${book.Author}</td>
+          <td>${book.ISBN}</td>
+        </tr>
+      `;
+    });
+  }
+
+  populateTable();
