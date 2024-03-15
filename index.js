baf2cdb01b207722205c2bf345bf155ab7eca994
Ally <allyy.noel@gmail.com>
2024-03-15 18:40:03 -0400

    start-over

--- index.js
+++ index.js
@@ -4,27 +4,15 @@ const supabaseUrl = 'https://hgviinnynoibggdyeynm.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhndmlpbm55bm9pYmdnZHlleW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MzA5MTMsImV4cCI6MjAyNjEwNjkxM30.CORJIYv0RdnkAQqtFj1hLzSyZ40BegrNUxvat9QJXHs'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function populateTable() {
+async function getBooks() {
   let { data: books, error } = await supabase
-    .from('books')
-    .select('title, author, isbn');
+  .from('books')
+  .select('*')
 
-  if (error) {
-    console.error('Error fetching books:', error.message);
-    return;
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<li>${book.title} - %{book.author} - ${book.isbn}</li>`;
   }
-
-  const tbody = document.getElementById('booksBody');
-
-  books.forEach(book => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.isbn}</td>
-    `;
-    tbody.appendChild(row);
-  });
 }
 
-populateTable();
+getBooks();
